73f52fdb3c0289eaad668399b3afd708b15d1802
andyberaldcatelo <andy.berald@yahoo.fr>
2020-10-05 08:04:57 +0200

    2020 10 05 Extracting Route Parameters

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -7,14 +7,15 @@ class FullPost extends Component {
     loadedPost: null,
   };
 
-  componentDidUpdate() {
-    if (this.props.id) {
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.props.id !== this.state.loadedPost.id)
+        (this.state.loadedPost && this.props.match.params.id !== this.state.loadedPost.id)
       ) {
         axios
-          .get('/posts/' + this.props.id)
+          .get('/posts/' + this.props.match.params.id)
           .then((response) => this.setState({ loadedPost: response.data }));
       }
     }
@@ -22,14 +23,14 @@ class FullPost extends Component {
 
   deletePostHandler = () => {
     axios
-      .delete('/posts/' + this.props.id)
+      .delete('/posts/' + this.props.match.params.id)
       .then((response) => console.log(response));
   };
 
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a post</p>;
 
-    if (this.props.id) {
+    if (this.props.match.params.id) {
       post = <p style={{ textAlign: 'center' }}>Loading...</p>;
     }
 
